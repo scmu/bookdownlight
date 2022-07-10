9d3ad7834c303976f80c169fbf28713bfa68f3ac
Shin-Cheng Mu <scm@iis.sinica.edu.tw>
2022-07-11 01:19:23 +0800

    [new] using shake the build system

--- app/Main.hs
+++ app/Main.hs
@@ -7,27 +7,46 @@ import Data.Text (Text)
 import Data.Text.Encoding (decodeUtf8)
 import qualified Data.ByteString as BS (ByteString, readFile)
 import qualified Data.Text.IO as TIO
+import Control.Monad (forM_)
 
 import Cheapskate
 import Book.TexRender
 
+import Development.Shake
+import Development.Shake.Command
+import Development.Shake.FilePath
+import Development.Shake.Util
+
 main :: IO ()
-main = -- generate lhs
-    do projBase <- getCurrentDirectory
-       putStr "base dir: "
-       putStr projBase
-       putStr "\n"
-       mapM_ (genLHs (projBase ++ "/")) chapters
+main = shakeArgs shakeOptions{shakeFiles="_build"} $ do
+
+        want ["tex/fpbook.tex"]
+
+        forM_ chapters (\ch ->
+          ("lhs/Chapters/" </> ch <.> "lhs") %> \lhsName -> do
+            let mdName = "contents" </> ch <.> "md"
+            need [mdName]
+            liftIO (genLHs mdName lhsName))
+
+        forM_ chapters (\ch ->
+          ("tex/Chapters/" </> ch <.> "tex") %> \texName -> do
+            let lhsName = "lhs/Chapters/" </> ch <.> "lhs"
+            need [lhsName]
+            command_ [Cwd "tex", FileStdout texName]
+               "lhs2TeX" [".." </> lhsName])
+
+        "tex/fpbook.tex" %> \out -> do
+           need (map (\ch -> "tex/Chapters/" </> ch <.> "tex") chapters)
+           command_ [Cwd "tex"]
+               "xelatex" ["fpbook"]
 
 genLHs :: String -> String -> IO ()
-genLHs base chname = do
+genLHs mdname lhsname = do
     hdl <- openFile lhsname WriteMode
     readFile lhsHeader >>= TIO.hPutStr hdl
     readFile mdname >>= handle hdl
     hClose hdl
-  where mdname  = contents base ++ chname ++ ".md"
-        lhsname = lhsChs base ++ chname ++ ".lhs"
-        lhsHeader = tmpls base ++ "lhsheader.lhs"
+  where lhsHeader = "templates/lhsheader.lhs"
 
 handle :: Handle -> Text -> IO ()
 handle h = texRender h . markdown def
@@ -36,17 +55,15 @@ readFile :: String -> IO Text
 readFile path = decodeUtf8 <$> BS.readFile path
 
 chapters :: [String]
-chapters = [ -- "Introduction"
-           -- ,"Basics"
-           -- ,"Induction"
-           -- ,
-           "Semantics"
+chapters = [ "Introduction"
+           , "Basics"
+           , "Induction"
+           , "Semantics"
            -- , "Derivation"
            -- , "Folds"
            ]
 -- paths
 
--- projBase = "/Users/scm/Documents/Repositories/bookdownlight/"
 contents projBase = projBase ++ "contents/"
 texBase  projBase = projBase ++ "tex/"
 lhsBase  projBase = projBase ++ "lhs/"
